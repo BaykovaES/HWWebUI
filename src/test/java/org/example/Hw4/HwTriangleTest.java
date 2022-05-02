145d1c228b7d189da33abc3c20ac2cfb8bbcdf61
BaykovaES <baykova.es@yandex.ru>
2022-05-02 22:42:02 +0300

    домашняя работа к четвертому уроку(исправления)

--- src/test/java/org/example/Hw4/HwTriangleTest.java
+++ src/test/java/org/example/Hw4/HwTriangleTest.java
@@ -2,23 +2,24 @@ package org.example.Hw4;
 
 import org.example.HWLesson4.MyException;
 import org.example.HWLesson4.Triangle;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.example.HWLesson4.Triangle.triangleArea;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HwTriangleTest {
     Logger logger = LoggerFactory.getLogger(HwTriangleTest.class);
 
     @Test
-    void test1() throws MyException {
+    public void areaExistingTriangle() throws MyException {
         logger.info("Тест 1: проверка сходимости формулы на существующем треугольнике");
         assertFalse(Triangle.triangleArea(3,6,7)==0);
         logger.info("\tТест успешен");
@@ -26,8 +27,7 @@ public class HwTriangleTest {
 
     @ParameterizedTest
     @CsvSource({"0,6,7","-3,-6,-7","3,-6,7"})
-
-    void test2(int a, int b, int c) throws MyException {
+    public void exceptionsTriangle(int a, int b, int c) throws MyException {
         logger.info("Тест 2: проверка исключения вычисления площади на треугольниках");
         logger.info("\tВходящие параметры - (a,b,c) - " +a+", "+b+", "+c);
         assertThrows(MyException.class, ()->Triangle.triangleArea(a, b, c));;
@@ -37,7 +37,7 @@ public class HwTriangleTest {
 
 
     @Test
-        void test3() throws MyException {
+     public void triangleNotExist() throws MyException {
         logger.info("Тест 3: проверка корректности срабатывания исключения, когда треугольник не существует");
         assertThrows(MyException.class, ()->Triangle.triangleArea(233, 6, 7));
         logger.info("\tТест успешен");
